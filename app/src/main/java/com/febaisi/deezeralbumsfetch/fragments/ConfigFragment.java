303ae61571f6ef87ecaa5ad2122e86174bd8f210
Felipe Baisi <febaisi@gmail.com>
2017-11-12 21:49:32 -0200

    Adding coordinator layout along with snack
    Signed-off-by: Felipe Baisi <febaisi@gmail.com>

--- app/src/main/java/com/febaisi/deezeralbumsfetch/fragments/ConfigFragment.java
+++ app/src/main/java/com/febaisi/deezeralbumsfetch/fragments/ConfigFragment.java
@@ -2,6 +2,8 @@ package com.febaisi.deezeralbumsfetch.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,18 +27,20 @@ public class ConfigFragment extends CustomFragment implements View.OnClickListen
     private Button mWipeCacheButton;
     private Button mAnimationButton;
     private EditText mAnimationEditText;
+    private CoordinatorLayout mRootCoordinatorLayout;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_config, container, false);
-        view.findViewById(R.id.config_main_layout).requestFocus();
+        view.findViewById(R.id.config_main_linear_layout).requestFocus();
 
         //Finding views
         mAnimationEditText = view.findViewById(R.id.config_anim_duration);
         mWipeCacheButton = view.findViewById(R.id.config_delete_cache);
         mSlowInternetToggleButton = view.findViewById(R.id.config_internet_slow);
         mAnimationButton = view.findViewById(R.id.config_anim_save);
+        mRootCoordinatorLayout = view.findViewById(R.id.config_root_layout);
 
         //Set click listeners
         mSlowInternetToggleButton.setOnClickListener(this);
@@ -68,21 +72,21 @@ public class ConfigFragment extends CustomFragment implements View.OnClickListen
             case R.id.config_internet_slow:
                 boolean checked = mSlowInternetToggleButton.isChecked();
                 if (checked) {
-                    Toast.makeText(getContext(), "Internet slow - Cache disabled", Toast.LENGTH_SHORT).show();
+                    Snackbar.make(mRootCoordinatorLayout, "Internet slow - Cache disabled", Snackbar.LENGTH_SHORT).show();
                 }
                 SharedPreferenceUtil.putBoolPref(getContext(), INTERNET_SLOW_PREF, checked);
                 break;
             case R.id.config_delete_cache:
-                Toast.makeText(getContext(), "Cache deleted", Toast.LENGTH_SHORT).show();
                 MemImageCache.getInstance().clearCache();
+                Snackbar.make(mRootCoordinatorLayout, "Cache deleted", Snackbar.LENGTH_SHORT).show();
                 break;
             case R.id.config_anim_save:
                 int newAnimationTime = Integer.parseInt(mAnimationEditText.getText().toString());
                 SharedPreferenceUtil.putIntPref(getContext(), CONFIG_ANIM_TIME, newAnimationTime);
-                Toast.makeText(getContext(), "New album cover fade in animation time: " + newAnimationTime, Toast.LENGTH_SHORT).show();
-
                 InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+
+                Snackbar.make(mRootCoordinatorLayout, "New album cover fade in animation time: " + newAnimationTime, Snackbar.LENGTH_SHORT).show();
                 break;
         }
 
